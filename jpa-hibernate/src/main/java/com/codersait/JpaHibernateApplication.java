0780df715200a62b847fc1c511bcd166c0a25b26
codersait <codersait@gmail.com>
2021-02-08 10:01:49 +0300

    jpa-hibernate added.

--- jpa-hibernate/src/main/java/com/codersait/JpaHibernateApplication.java
+++ jpa-hibernate/src/main/java/com/codersait/JpaHibernateApplication.java
@@ -1,7 +1,14 @@
 package com.codersait;
 
+import com.codersait.repository.StudentRepository;
+import com.codersait.repository.StudentRepositoryImpl;
+import com.codersait.service.StudentService;
+import com.codersait.service.StudentServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
 public class JpaHibernateApplication {
--- jpa-hibernate/src/main/java/com/codersait/repository/StudentRepository.java
+++ jpa-hibernate/src/main/java/com/codersait/repository/StudentRepository.java
@@ -5,6 +5,7 @@ import com.codersait.model.Student;
 import java.util.List;
 
 public interface StudentRepository {
+
     public List<Student> findAll();
     public void save(Student student);
     public void delete(Integer id);
